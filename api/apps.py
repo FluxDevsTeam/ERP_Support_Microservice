0e735acad599718c66ed3fd2ed78d0034b3596ae
Masodi Success <109852676+suskidee@users.noreply.github.com>
2025-11-10 13:21:46 +0100

    feat(api): Add api app config and blogs URL pattern in v1
    Add the api app configuration (__init__.py and apps.py) to set up the Django app.
    Include the blogs URL pattern in the v1 API URLs to provide access to the blogs service.

---
+++ api/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
+    verbose_name = 'API'
+    path = 'c:\\Users\\HomePC\\Desktop\\FluxDevs\\ERP_SAAS\\ERP_Support_Microservice\\api'
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("email-service/", include("apps.email_service.urls")),
+    path("blogs/", include("apps.blogs.urls")),
 ]
